7ef211c00b1723fe0d5f563dac5087eb918480d3
eunicean <eunice.mata2992@gmail.com>
2025-02-21 23:36:59 -0600

    exercise 5

--- runner.c
+++ runner.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
-    pid_t pid = fork();
+  pid_t pid = fork();
 
-    if (pid < 0) {
-        perror("Error en fork");
-        exit(EXIT_FAILURE);
-    }
+  if (pid == 0) {
+    execl("./ipc", "ipc", "5", "a", NULL);
+    perror("Error en execl");
+  } else {
+    execl("./ipc", "ipc", "5", "b", NULL);
+    perror("Error en execl");
+  }
 
-    if (pid == 0) {
-        // Proceso hijo - Ejecutar ipc con "A"
-        execl("./ipc", "ipc", "5", "A", NULL);
-        perror("Error en execl");
-    } else {
-        // Proceso padre - Ejecutar ipc con "B"
-        execl("./ipc", "ipc", "5", "B", NULL);
-        perror("Error en execl");
-    }
-
-    return 0;
+  return 0;
 }
