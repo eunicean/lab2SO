553f93cf9102df93a3aa9b657a76b9cb0b5ff4da
eunicean <eunice.mata2992@gmail.com>
2025-02-21 22:30:06 -0600

    exercise 4

--- e41.c
+++ e41.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <time.h>
 #include <sys/wait.h>
 
 int main(){
   pid_t p = fork();
-  if(p==0) for(int i=0;i<50000000; i++) printf("%d\n",i);
+  
+  if(p==0) printf("pid %d\n", getpid());
   else while(1);
   
   return(0);
