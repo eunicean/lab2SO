204eeeeb85cc10a3ea24c50683e10ac858cd4ade
eunicean <eunice.mata2992@gmail.com>
2025-02-21 22:10:14 -0600

    exercise 4

--- e44.c
+++ e44.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include <sys/wait.h>
 
 int main(){
   pid_t p = fork();
-
-  if(p==0) for(int i=0;1000; i++) printf("%d\n",i);
+  clock_t inicio, fin;
+  double tiempo;
+  
+  inicio = clock();
+  if(p==0) for(int i=0;i<1600000; i++) printf("%d\n",i);
   else while(1);
+  fin = clock();
+  tiempo = (double)(fin-inicio);
+  printf("Tiempo: %f segundos\n", tiempo/CLOCKS_PER_SEC);
   
   return(0);
 }
